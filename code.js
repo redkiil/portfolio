698a0bd3ea7a7693155eadba634e943953d9df4a
redkiil <redkiil@hotmail.com>
2022-10-16 00:35:48 -0300

    feat: mobile version

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var windowWidth = window.screen.width < window.outerWidth ?
+    window.screen.width : window.outerWidth;
+    var mobile = windowWidth < 500;
+    if(!mobile){
     let handle = 0;
     let elements = document.querySelectorAll("[data-project]")
     let button = document.querySelector(".btn-whoiam")
@@ -41,6 +45,26 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(() => {
         document.querySelector('.mousescroll').classList.remove('mousescroll-show') 
     }, 5000);
+    }else{
 
+        let mobileMenu = document.querySelector('.mobile-menu')
+        let closeMenu = document.querySelector('.close-menu')
+        let menu = document.querySelector('.menu')
+        let button = document.querySelector(".btn-whoiam")
+        let modal = document.querySelector(".modal")
 
+        mobileMenu.onclick = function(){
+            menu.style.display = "flex"
+        }
+        closeMenu.onclick = function(){
+            menu.style.display = "none"
+
+        }
+
+        button.onclick = function() {
+            menu.style.display = "none"
+            modal.style.display = "block"
+        }
+
+    }
  }, false);
