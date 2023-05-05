316aa08be581119a4c7bac7e84859e8e0b6fa08e
unknown <dev.augusto.ferreira@gmail.com>
2023-05-05 18:24:58 -0300

    changes in language icon

--- code/animations.js
+++ code/animations.js
@@ -1,10 +1,13 @@
-import { closeMenu, menu } from "./selectors.js"
+import { closeMenu, menu, langIcon } from "./selectors.js"
 
 export function toggleMobileMenu(){
     if(menu.style.display == "none" || menu.style.display == ""){
         menu.style.display = "flex"
         closeMenu.style.display = "block"
+        langIcon.style.display = "block"
     }else{
         menu.style.display = "none"
+        langIcon.style.display = "none"
+
     }
 }
